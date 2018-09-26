811747330edbade866b1a44f7fb23da5d132a8a3
Ishita <Ishita@LAPTOP-DRUPB3O0>
2018-09-26 15:22:43 -0500

    Red Light |

--- RCube/test/createCubeTest.py
+++ RCube/test/createCubeTest.py
@@ -2,8 +2,16 @@ import unittest
 import RCube.dispatch as RCube
 
 class createCubeTest(unittest.TestCase):
-    def test100_610_ShouldCreateOneElementCube(self):
+
+    # def test100_610_ShouldCreateOneElementCube(self):
+    #     parm = {'op' : 'create'}
+    #     expectedResult = ['green']
+    #     actualResult = RCube.createCube(parm)
+    #     self.assertListEqual(expectedResult, actualResult)
+    
+    def test100_620_ShouldCreateMultipleElementCube(self):
         parm = {'op' : 'create'}
         expectedResult = ['green']
         actualResult = RCube.createCube(parm)
-        self.assertListEqual(expectedResult, actualResult)
+        for elementIndex in range(0,9):
+            self.assertEqual(expectedResult, actualResult[elementIndex])
