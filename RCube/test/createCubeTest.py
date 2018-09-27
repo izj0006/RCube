2e0d8e184bae238cb5d0df454368b8f5efbaa926
Ishita <Ishita@LAPTOP-DRUPB3O0>
2018-09-26 20:02:56 -0500

    Green Light |

--- RCube/test/createCubeTest.py
+++ RCube/test/createCubeTest.py
@@ -18,7 +18,7 @@ class createCubeTest(unittest.TestCase):
 
     def test100_630_ShouldCreateMultipleFaceCube(self):
         parm={'op':'create','f':'f','r':'r','b':'b','l':'l','t':'t','u':'u'}
-        expectedFaces = ['green', 'yellow', 'blue', 'white', 'red', 'orange']
+        expectedFaces = ['f', 'r', 'b', 'l', 't', 'u']
         actualResult= RCube.createCube(parm)
         elementIndex=0
         for face in expectedFaces:
