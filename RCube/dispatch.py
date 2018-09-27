c2be57cd651cf1943c626c8a092fb46c7c417ff8
Ishita <Ishita@LAPTOP-DRUPB3O0>
2018-09-26 19:56:56 -0500

    Green Light |

--- RCube/dispatch.py
+++ RCube/dispatch.py
@@ -5,50 +5,8 @@ def dispatch(parm={}):
         httpResponse['status'] = 'error: missing op'
     elif(parm['op'] == 'create'):
         httpResponse['status'] = 'created'
-        #httpResponse['cube']=createCube(parm)
-        cube=['green','green','green',
-          'green','green','green',
-          'green','green','green',
-          'yellow','yellow','yellow',
-          'yellow','yellow','yellow',
-          'yellow','yellow','yellow',
-          'blue','blue','blue',
-          'blue','blue','blue',
-          'blue','blue','blue',
-          'white','white','white',
-          'white','white','white',
-          'white','white','white',
-          'red','red','red',
-          'red','red','red',
-          'red','red','red',
-          'orange','orange','orange',
-          'orange','orange','orange',
-          'orange','orange','orange']
-    return cube
-    #return httpResponse
-    
-    if(parm['op'] == 'create' & parm['f']=='f'):
-        httpResponse['status'] = 'created'
-        #httpResponse['cube']=createCube(parm)
-        cube=['f','f','f',
-          'f','f','f',
-          'f','f','f',
-          'yellow','yellow','yellow',
-          'yellow','yellow','yellow',
-          'yellow','yellow','yellow',
-          'blue','blue','blue',
-          'blue','blue','blue',
-          'blue','blue','blue',
-          'white','white','white',
-          'white','white','white',
-          'white','white','white',
-          'red','red','red',
-          'red','red','red',
-          'red','red','red',
-          'orange','orange','orange',
-          'orange','orange','orange',
-          'orange','orange','orange']
-    return cube
+        httpResponse['cube']=createCube(parm)
+    return httpResponse
     
 def createCube(parm):
     cube=['green','green','green',
@@ -70,3 +28,4 @@ def createCube(parm):
           'orange','orange','orange',
           'orange','orange','orange']
     return cube
+
