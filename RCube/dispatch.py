0036d44837ac81a988788794d971770ea84655a2
Ishita <Ishita@LAPTOP-DRUPB3O0>
2018-09-26 19:51:11 -0500

    Green Light |

--- RCube/dispatch.py
+++ RCube/dispatch.py
@@ -5,8 +5,27 @@ def dispatch(parm={}):
         httpResponse['status'] = 'error: missing op'
     elif(parm['op'] == 'create'):
         httpResponse['status'] = 'created'
-        httpResponse['cube']=createCube(parm)
-    return httpResponse
+        #httpResponse['cube']=createCube(parm)
+        cube=['green','green','green',
+          'green','green','green',
+          'green','green','green',
+          'yellow','yellow','yellow',
+          'yellow','yellow','yellow',
+          'yellow','yellow','yellow',
+          'blue','blue','blue',
+          'blue','blue','blue',
+          'blue','blue','blue',
+          'white','white','white',
+          'white','white','white',
+          'white','white','white',
+          'red','red','red',
+          'red','red','red',
+          'red','red','red',
+          'orange','orange','orange',
+          'orange','orange','orange',
+          'orange','orange','orange']
+    #return httpResponse
+    return cube
     
 def createCube(parm):
     cube=['green','green','green',
@@ -27,23 +46,4 @@ def createCube(parm):
           'orange','orange','orange',
           'orange','orange','orange',
           'orange','orange','orange']
-    return cube
-
-def createCubeOne(parm):
-    cube=['f','f','f',
-          'f','f','f',
-          'f','f','f',
-          'r','r','r',
-          'r','r','r',
-          'r','r','r',
-          'b','b','b',
-          'b','b','b',
-          'b','b','b',
-          'l','l','l',
-          'l','l','l',
-          'l','l','l',
-          't','t','t',
-          't','t','t',
-          't','t','t',
-          'u','u','u']
     return cube
