c5f1a0be82d3e7a154332920404b128a7d7ace2b
Ishita <Ishita@LAPTOP-DRUPB3O0>
2018-10-13 13:20:32 -0500

    Green Light |

--- RCube/dispatch.py
+++ RCube/dispatch.py
@@ -1,24 +1,49 @@
+front = 'green'
+right = 'yellow'
+bottom = 'blue'
+left = 'white'
+top = 'red'
+under = 'orange'
+
+faces = [front, right, bottom, left, top, under]
+
 def dispatch(parm={}):
     httpResponse = {}
     if(not('op' in parm)):
         httpResponse['status'] = 'error: missing op'
+        
     elif(parm['op'] == 'create'):
-        httpResponse['status'] = 'created'
-        httpResponse['cube']=createCube(parm)
+        response = createCube(parm)
+        if(response != 'error: duplicate faces'):
+            httpResponse['status'] = 'created'
+            httpResponse['cube']=response
         
     elif(parm['op'] == 'check'):
         if(not('cube' in parm)):
-            httpResponse['status'] = 'error: cube must be specified'        
+            httpResponse['status'] = 'error: cube must be specified'
+        else:
+            response = checkSize(parm)
+            if(response != 'error: cube is not sized properly'):
+                httpResponse['status'] = 'size checked'
+                httpResponse['cube']=response
+            
     return httpResponse
+
+def checkDupeColors(parm):
+    for indexFace in range(0, 6):
+        for indexFace2 in range(0, 6):
+            if (indexFace != indexFace2):
+                if faces[indexFace] == faces[indexFace2]:
+                    error_message = 'error: duplicate faces'
+                    return error_message
     
-def createCube(parm):
-    front = 'green'
-    right = 'yellow'
-    bottom = 'blue'
-    left = 'white'
-    top = 'red'
-    under = 'orange'
+def checkSize(parm):
+    #check length of cube
+        #if less than 54
+            #    httpResponse['status'] = 'error: cube is not sized properly'
+    return 
     
+def createCube(parm):   
     if('f' in parm):
         front = parm['f']
     if('r' in parm):
@@ -31,17 +56,14 @@ def createCube(parm):
         top = parm['t']
     if('u' in parm):
         under = parm['u']
-        
-    faces = [front, right, bottom, left, top, under]   
-    for indexFace in range(0, 6):
-        for indexFace2 in range(0, 6):
-            if (indexFace != indexFace2):
-                if faces[indexFace] == faces[indexFace2]:
-                    error_message = 'error: duplicate faces'
-                    return error_message
-    cube = []
-    for face in faces:
-        for _ in range(0,9):
-            cube.append(face)
+
+    response = checkDupeColors(parm)
+    
+    if(response != 'error: duplicate faces'):
+        cube = []
+        for face in faces:
+            for _ in range(0,9):
+                cube.append(face)
+                    
     return cube
 
