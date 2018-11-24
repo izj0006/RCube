074863aeb7dc130e010907bfd85b41fd24ad106c
Ishita <Ishita@LAPTOP-DRUPB3O0>
2018-11-24 15:43:04 -0600

    Red Light |

--- RCube/dispatch.py
+++ RCube/dispatch.py
@@ -5,7 +5,7 @@ left = 'white'
 top = 'red'
 under = 'orange'
 faces = [front, right, bottom, left, top, under]
-frbltu
+
 face = ['f','F','r','R','b','B','l','L','t','T','u','U']
 edges = []
 middle = []
@@ -72,11 +72,11 @@ def determineConfig(parm):
     if( splitcube[0].count(splitcube[0][1]) == 9 & splitcube[1].count(splitcube[1][1]) == 9 & splitcube[2].count(splitcube[2][1]) == 9 & splitcube[3].count(splitcube[3][1]) == 9 & splitcube[4].count(splitcube[4][1]) == 9 & splitcube[5].count(splitcube[5][1]) == 9 ):
         message = 'FULL'
     
-    elif(cube='r,w,r,w,w,w,r,w,r, w,g,w,g,g,g,w,g,w,o,y,o,y,y,y,o,y,o, y,b,y,b,b,b,y,b,y, g,r,g,r,r,r,g,r,g, b,o,b,o,o,o,b,o,b'):
+    elif(cube=='r,w,r,w,w,w,r,w,r, w,g,w,g,g,g,w,g,w,o,y,o,y,y,y,o,y,o, y,b,y,b,b,b,y,b,y, g,r,g,r,r,r,g,r,g, b,o,b,o,o,o,b,o,b'):
         #cube[0] == cube[2] == cube[6] == cube[8] & cube[1] == cube[3]== cube[4]== cube[5]== cube[7]
         message = 'CROSSES'
         
-    elif(cube='y,y,y,y,r,y,y,y,y, o,o,o,o,b,o,o,o,o,w,w,w,w,o,w,w,w,w,r,r,r,r,g,r,r,r,r,b,b,b,b,w,b,b,b,b, g,g,g,g,y,g,g,g,g'):
+    elif(cube=='y,y,y,y,r,y,y,y,y, o,o,o,o,b,o,o,o,o,w,w,w,w,o,w,w,w,w,r,r,r,r,g,r,r,r,r,b,b,b,b,w,b,b,b,b, g,g,g,g,y,g,g,g,g'):
         message = 'SPOTS'
         
     else:
